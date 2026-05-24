53936e88cfb12728ce0452269cd2eb76b74f0d3c
Arefat <262428690+Arefat21@users.noreply.github.com>
2026-05-24 10:13:41 +0300

    chore: remove comments

--- rps.js
+++ rps.js
@@ -11,9 +11,7 @@ function getComputerChoice(){
         return "scissors";
     }
 }
-// The Variable randomNumber and humanInput should be inside the function because:
-// Outside the function: Runs once when the page loads. The value never changes.
-// Inside the function: Runs every single time the function is called. You get a fresh input every round.
+
 
 function getHumanChoice(){
     const humanInput = prompt("Enter rock, paper, or scissors:");
@@ -47,22 +45,13 @@ let playRound = function(humanSelection, computerSelection){
 function playGame(){
     let humanScore = 0;
     let computerScore = 0;
-    /* If we decared the variable humanScore and computerScore globally "game 1" would work perfectly 
-    fine the first time we call the function "playGAme", but when we call the function again to play "game 2"
-    it would started from the score where it was. It wouldn't reset to zero.
-    */ 
+     
     for (let i = 0; i < 5; i++){
-        /* For loop works like while i=0 it executes everything in it, then increment i by one and i became 1 and again executes everything inside
-        and at the end when i=5 which is not < 5 and the loop stops here and exit.
-        */
 
         let humanChoice = getHumanChoice();
         let computerChoice = getComputerChoice();
-        // We call the functions HERE to get fresh choices for each round.
-        // The two variables(humanChoice and ComputerChoice() stores the output of each function.
-            
         let result = playRound(humanChoice, computerChoice);
-        // We called the playRound function here and stored the output with variable "result".
+
         console.log(`Round: ${i + 1}`);
         console.log(`computer chose: ${computerChoice}`);
         console.log(`You chose: ${humanChoice}`);
@@ -74,7 +63,7 @@ function playGame(){
         } else if (result === "You lose!"){
             computerScore++;
         }
-
+        
         console.log(`Current Score = Human: ${humanScore} | Computer: ${computerScore}`);
 
     }
